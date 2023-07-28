4477f4d5bfb9f9b7cf3d33a5644db928611b5774
a-c-hawken <a.c.hawken@student.saintkentigern.com>
2023-07-28 13:37:20 +1200

    music

--- iPadCarInfotainment/MusicViewController.swift
+++ iPadCarInfotainment/MusicViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import MusicKit
+import StoreKit
 
 class MusicViewController: UIViewController {
 
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var albumArt: UIImageView!
+    
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var songDurationSlider: UISlider!
+    @IBOutlet weak var playingNextTableView: UITableView!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    struct Song {
+        var id: String
+        var name: String
+        var artistName: String
+        var artworkURL: String
+     
+        init(id: String, name: String, artistName: String, artworkURL: String) {
+            self.id = id
+            self.name = name
+            self.artworkURL = artworkURL
+            self.artistName = artistName
+        }
+    }
+    
+    var upnextSongs: [Song] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func volumeSliderChanged(_ sender: Any) {
+        let volume = volumeSlider.value
+    }
+    
+
+
+    @IBAction func playPauseButtonPressed(_ sender: Any) {
+        
+    }
+    
+    
     
 
     /*
---
+++ iPadCarInfotainment/UpNextUITableView.swift
@@ -0,0 +1,20 @@
+//
+//  UpNextUITableView.swift
+//  iPadCarInfotainment
+//
+//  Created by Aydan H on 28/07/23.
+//
+
+import UIKit
+
+class UpNextUITableView: UITableView {
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
