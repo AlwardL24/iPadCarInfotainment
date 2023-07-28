95846efb332087cb7406da4d082b397cf1e9cf83
Lucas Alward <73626623+AlwardL24@users.noreply.github.com>
2023-07-28 12:05:52 +1200

    Initial Commit

---
+++ iPadCarInfotainment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPadCarInfotainment
+//
+//  Created by Lucas Alward on 28/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
